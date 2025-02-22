c9a2e81a7e4fb3da4e9eb6da26e4990663c8f969
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2025-02-22 08:19:12 +0000

    Site updated: 2025-02-22 08:19:12

---
+++ js/random.js
@@ -0,0 +1,1 @@
+var posts=["posts/164ef646/","posts/e54f7476/","posts/ad884987/","posts/c81531cf/","posts/a9a6c1fb/","posts/de1f83f4/","posts/c262e439/","posts/1ee472c8/","posts/228c2ef8/","posts/2b89093e/","posts/ce5471fd/","posts/04f3c137/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
